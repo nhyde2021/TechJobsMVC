c43e82d150ca1647da7af1b55b507786bb33af23
Neil Hyde <neil.hyde17@gmail.com>
2022-01-03 14:27:21 -0600

    added All category to list/index

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -20,6 +20,7 @@ namespace TechJobsMVC.Controllers
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+        
         public IActionResult Results(string searchType, string searchTerm)
         {
             List<Job> jobs = new List<Job>();
